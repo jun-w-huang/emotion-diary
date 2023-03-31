f52bface6fed7f5420241a53fc3cd83be5f18eec
jun-w-huang <93484080+jun-w-huang@users.noreply.github.com>
2023-03-31 17:31:55 -0400

    Added calendar and updated Prisma Model

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { emotionEventRouter } from "~/server/api/routers/emotionEvent";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  emotionEvent: emotionEventRouter,
 });
 
 // export type definition of API
