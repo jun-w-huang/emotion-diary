a4dbd1cca98736bc0c8fb0dab0283b35457add74
jun-w-huang <93484080+jun-w-huang@users.noreply.github.com>
2023-06-22 10:21:21 +0800

    Moved icon to be a prop rather than child of EmotionButton

--- src/components/EmotionButton.tsx
+++ src/components/EmotionButton.tsx
@@ -1,21 +1,23 @@
-import React, { ButtonHTMLAttributes, forwardRef } from "react";
+import React, { ButtonHTMLAttributes, ReactNode, forwardRef } from "react";
 
 export interface EmotionButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
   label?: string;
   isActive?: boolean;
+  icon?: ReactNode;
 }
 
 export const EmotionButton = forwardRef<HTMLButtonElement, EmotionButtonProps>(
   ({ isActive, ...props }, ref) => (
     <button
-      className={`flex h-12 w-32 items-center rounded-lg pl-2 ${
-        isActive ? "text-emotionDarkBlue bg-white" : "text-emotionGray"
+      className={`flex h-12 w-32 flex-row items-center rounded-lg pl-2 gap-2 ${
+        isActive ? "bg-white text-emotionDarkBlue" : "text-emotionGray"
       }`}
       {...props}
       ref={ref}
     >
-      <h2 className="flex flex-row items-center gap-2">{props.children}</h2>
+      {props.icon ? props.icon : undefined}
+      <h2>{props.children}</h2>
     </button>
   )
 );
