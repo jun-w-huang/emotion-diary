267ec0bcd416e5744abe2eb4e107124f5f1c6e5f
jun-w-huang <93484080+jun-w-huang@users.noreply.github.com>
2023-07-07 09:41:11 -0400

    styling

--- src/components/RHF/InputFieldRHF.tsx
+++ src/components/RHF/InputFieldRHF.tsx
@@ -7,7 +7,7 @@ const InputFieldRHF = React.forwardRef<
   <input
     {...props}
     ref={ref}
-    className="w-full rounded-md bg-emotionLightGray p-3 text-white"
+    className="w-full rounded-md bg-emotionLightGray p-3 text-black"
   />
 ))
 InputFieldRHF.displayName = "TextField";
