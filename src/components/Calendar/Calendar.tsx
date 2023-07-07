3768cc7e6990ee7da59475d9fe5f3e14230b2c16
jun-w-huang <93484080+jun-w-huang@users.noreply.github.com>
2023-07-07 14:49:48 -0400

    Added useEffect to initially set DetailedDayModal

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { EmotionEvent } from "@prisma/client";
 import React from "react";
 import CalendarMonthlyView from "./Monthly/CalendarMonthlyView";
@@ -6,6 +6,8 @@ import CalendarWeeklyView from "./Weekly/CalendarWeeklyView";
 import { CalendarNavbar } from "./CalendarNavbar";
 import AddSVG from "../../../public/plus.svg";
 import { useEmotionRHFModalContext } from "~/context/EmotionRHFModalContext";
+import { useDetailedDayModalContext } from "~/context/DetailedDayModalContext";
+import { format } from "date-fns";
 
 interface CalendarProps {
   events: EmotionEvent[];
@@ -19,6 +21,26 @@ const Calendar = (props: CalendarProps) => {
   const [viewMode, setViewMode] = useState<"month" | "week">("month");
 
   const { dispatch } = useEmotionRHFModalContext();
+
+  const { dispatch : DDMDispatch } = useDetailedDayModalContext();
+
+  // this useEffect runs only when the Calendar first is opened and will set the DetailedDayModal
+  // to display today's date.
+  useEffect(() => {
+    console.log("useeffect")
+    //find today's events
+    const dayEvents = props.events.filter(
+      (event) =>
+        format(event.start, "MM/dd/yyyy") ===
+        format(new Date(), "MM/dd/yyyy")
+    );
+
+    DDMDispatch({
+      type: "open selected",
+      date: new Date(),
+      dateEvents: dayEvents,
+    })
+  }, [])
 
   return (
     <div className="flex max-h-full w-full flex-1 flex-col bg-white p-4">
