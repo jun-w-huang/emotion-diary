b33b9c7c706b701ad56d04378aeb089e081c9dc5
jun-w-huang <93484080+jun-w-huang@users.noreply.github.com>
2024-03-18 14:13:59 -0400

    fix lint

--- src/components/EmotionButton.tsx
+++ src/components/EmotionButton.tsx
@@ -10,7 +10,7 @@ export interface EmotionButtonProps
 export const EmotionButton = forwardRef<HTMLButtonElement, EmotionButtonProps>(
   ({ label, className, ...props }, ref) => (
     <button
-      className={`flex h-10 w-24 items-center justify-center rounded-2xl bg-emotionDarkBlue text-white ${className}`}
+      className={`flex h-10 w-24 items-center justify-center rounded-2xl bg-emotionDarkBlue text-white ${className || ""}`}
       {...props}
       ref={ref}
     >
